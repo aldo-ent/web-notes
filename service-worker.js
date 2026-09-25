de3c5ff6a9078c6de253986be4674a7b6ec58d0b
aldo-ent <grcss.alejandro@gmail.com>
2026-09-25 23:20:02 +0200

    ajout de md dans japonais grammaire.

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ⚠️ IMPORTANT : change ce numéro de version à CHAQUE fois que tu modifies
 // l'app (html/css/js). C'est ce qui force le téléphone à télécharger
 // la nouvelle version au lieu de garder l'ancienne en cache.
-const CACHE_NAME = "mon-app-v6";
+const CACHE_NAME = "mon-app-v7";
 
 const FILES_TO_CACHE = [
   "./",
